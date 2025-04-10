f58879e01e6a89134373e0b1556e89dd4173e4cf
hexalink-net <edwardsuryaputra@gmail.com>
2025-04-10 15:18:08 +0700

    add checkout

---
+++ src/app/checkout/[priceid]/page.tsx
@@ -0,0 +1,18 @@
+import { CheckoutHeader } from '@/components/checkout/CheckoutHeader';
+import { CheckoutContents } from '@/components/checkout/CheckoutContents';
+import { createClient } from '@/lib/supabase/server';
+
+export default async function CheckoutPage() {
+  const supabase = await createClient();
+  const { data } = await supabase.auth.getUser();
+  return (
+    <div className={'w-full min-h-screen relative overflow-hidden'}>
+      <div
+        className={'mx-auto max-w-6xl relative px-[16px] md:px-[32px] py-[24px] flex flex-col gap-6 justify-between'}
+      >
+        <CheckoutHeader />
+        <CheckoutContents userEmail={data.user?.email} />
+      </div>
+    </div>
+  );
+}
