da97cf4104e17eb1e6cc62c580da778344663f8d
hexalink-net <osmium76app@gmail.com>
2025-04-24 13:09:08 +0700

    fix build

--- src/app/(protected)/account/page.tsx
+++ src/app/(protected)/account/page.tsx
@@ -1,4 +1,3 @@
-import { createClient } from '@/lib/supabase/server';
 import { fetchAccountData } from './actions';
 import { UserMenu } from '@/components/UserMenu';
 import { redirect } from 'next/navigation';
